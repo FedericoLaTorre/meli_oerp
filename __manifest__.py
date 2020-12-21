a6148406d0c452e869464eaf86c92c554a8cf243
FedericoLaTorre <fedelatorre@gmail.com>
2020-12-21 17:56:18 -0300

    update 84

--- __manifest__.py
+++ __manifest__.py
@@ -8,7 +8,7 @@
 
 {
     'name': 'MercadoLibre Publisher',
-    'version': '12.0.20.75',
+    'version': '12.0.20.84',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
